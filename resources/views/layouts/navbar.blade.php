41c317e772726f79116e6378cc893c1123b1e33d
Anthony Chan <anthonychan@live.nl>
2022-09-08 09:36:46 +0200

    Navbar finish
    Finished the navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,12 +1,15 @@
+{{--
 <!-- top bar -->
 <header>
-    <div class="top-bar" data-scroll data-scroll-sticky data-scroll-target="#content" data-scroll-offset="-10">
+    <div class="top-bar">
         <div class="container">
+
             <div class="tb-left-side">
                 <!-- logo -->
                 <a href="{{ route('pages.index') }}" class="logo-frame">
-                    <img src=" {{ asset('assets/img/hz_logo.png') }}" alt="HZ logo">
+                    <img src=" {{ asset('assets/img/hz_logo.png') }}">
                 </a>
+                <!-- logo end -->
             </div>
         </div>
         <!-- logo end -->
@@ -40,7 +43,47 @@
         </div>
     </div>
     <div class="tb-menu-btn"><span></span></div>
-    </div>
-    </div>
 </header>
 <!-- top bar end -->
+--}}
+
+<header>
+    <div class="top-bar">
+        <div class="container">
+            <div class="tb-left-side">
+                <!-- logo -->
+                <a href="{{ route('pages.index') }}" class="logo-frame">
+                    <img src=" {{ asset('assets/img/hz_logo.png') }}">
+                </a>
+                <!-- logo end -->
+            </div>
+            <div class="tb-right-side">
+
+                <!-- menu -->
+                <div class="tb-menu">
+                    <nav>
+                        <ul>
+                            <li id="home-link" class="current-item">
+                                <a href="#home-triger" data-scroll-to="#home-triger" data-scroll-offset="-10">Home</a>
+                            </li>
+                            <li id="about-link">
+                                <a href="#about-triger" data-scroll-to="#about-triger" data-scroll-offset="-130">Profiel</a>
+                            </li>
+                            <li id="portfolio-link">
+                                <a href="#portfolio-triger" data-scroll-to="#portfolio-triger" data-scroll-offset="-130">Dashboard</a>
+                            </li>
+                            <li id="resume-link">
+                                <a href="#resume-triger" data-scroll-to="#resume-triger" data-scroll-offset="-130">FAQ</a>
+                            </li>
+                            <li id="contact-link">
+                                <a href="#contact-triger" data-scroll-to="#contact-triger" data-scroll-offset="-130">Blog</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+                <!-- menu end -->
+            </div>
+            <div class="trm-menu-btn"><span></span></div>
+        </div>
+    </div>
+</header>
